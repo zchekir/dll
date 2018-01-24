d18a879d7a03275d7929d3f13f4e18604a7d7270
Frank Barilla <fbarilla@cogstate.com>
2018-01-24 17:05:54 +1100

    Folder structure.

--- _assets/engine/AssemblyInfo.cs
+++ _assets/engine/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RCC")]
+[assembly: AssemblyTitle("engine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RCC")]
+[assembly: AssemblyProduct("engine")]
 [assembly: AssemblyCopyright("Copyright 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
