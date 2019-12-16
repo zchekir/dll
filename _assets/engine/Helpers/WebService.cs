93e08a4afba9ab5e2828c7ec6eec40e48742e136
Darren Wood <dwood@cogstate.com>
2019-12-16 14:12:01 +1100

    QC-41 - Fix in engine for WorkflowInstanceURL not launching test

--- _assets/engine/Helpers/WebService.cs
+++ _assets/engine/Helpers/WebService.cs
@@ -475,7 +475,7 @@ namespace engine.Helpers
         public static void LaunchAssessment(string Browser)
         {
         	//URL requires encoding as it may contain spaces when it is retrieved from the API
-        	AssessmentURL = Uri.EscapeUriString(AssessmentURL);
+        	//AssessmentURL = Uri.EscapeUriString(AssessmentURL);
         	Report.Log(ReportLevel.Info, "Website", "Opening web site " + AssessmentURL + " with browser " + Browser + " in normal mode.");
         	Host.Current.OpenBrowser(AssessmentURL, Browser, "", false, false, false, false, false, true);
         }
