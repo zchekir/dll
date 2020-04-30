e16f4cb44cd0a01ef5c4fdb94f0d6fc4733bdf4e
Zak Chekir <zchekir@cogstate.com>
2020-04-30 10:12:51 -0400

    QC-Fixed API bug-

--- eSourcePlatform/CSP/APIV2Modules/RrunWorkflow.UserCode.cs
+++ eSourcePlatform/CSP/APIV2Modules/RrunWorkflow.UserCode.cs
@@ -37,7 +37,7 @@ namespace CSP.APIV2Modules
             // Your recording specific initialization code goes here.
         }
 
-        public void R_runworkflow(string AuthToken, string studyProtocolName, string DOM, string genderCode, string localityCode, string postError, string post, string visitSessionCode, string supervisorid, string TestIdentifier, string externalid, string mode)
+        public void R_runworkflow(string AuthToken, string studyProtocolName, string DOM, string genderCode, string localityCode, string redirectError, string redirect, string visitSessionCode, string supervisorid, string TestIdentifier, string externalid, string mode)
         {
             //variable
     	string url = "https://"+ DOM +"/api/external/V2/AutomationStudy/AssessmentAttempt";
@@ -51,7 +51,7 @@ namespace CSP.APIV2Modules
              string dob ="1978-01-01";   
         		
         	//Create JSON object containing demographics and study details
-        	AssessmentAttemptJSONRequest assessmentObject = new AssessmentAttemptJSONRequest(externalid, dob, genderCode, localityCode, postError, post, visitSessionCode, supervisorid,mode);
+        	AssessmentAttemptJSONRequest assessmentObject = new AssessmentAttemptJSONRequest(externalid, dob, genderCode, localityCode, redirectError, redirect, visitSessionCode, supervisorid,mode);
         
         	
         	using (StreamWriter sw = new StreamWriter(httpRequest.GetRequestStream()))
--- eSourcePlatform/CSP/APIV2Modules/RrunWorkflow.cs
+++ eSourcePlatform/CSP/APIV2Modules/RrunWorkflow.cs
@@ -46,8 +46,8 @@ namespace CSP.APIV2Modules
             DOM = "";
             genderCode = "";
             localityCode = "";
-            postError = "";
-            post = "";
+            redirectError = "";
+            redirect = "";
             visitSessionCode = "";
             supervisorid = "";
             TestIdentifier = "";
@@ -125,28 +125,28 @@ namespace CSP.APIV2Modules
             set { _localityCode = value; }
         }
 
-        string _postError;
+        string _redirectError;
 
         /// <summary>
-        /// Gets or sets the value of variable postError.
+        /// Gets or sets the value of variable redirectError.
         /// </summary>
         [TestVariable("9890c6b6-58d3-4e7b-9fcc-8b647e1af4b3")]
-        public string postError
+        public string redirectError
         {
-            get { return _postError; }
-            set { _postError = value; }
+            get { return _redirectError; }
+            set { _redirectError = value; }
         }
 
-        string _post;
+        string _redirect;
 
         /// <summary>
-        /// Gets or sets the value of variable post.
+        /// Gets or sets the value of variable redirect.
         /// </summary>
         [TestVariable("e71ec1da-f47d-4dfa-ab50-156efac5f95a")]
-        public string post
+        public string redirect
         {
-            get { return _post; }
-            set { _post = value; }
+            get { return _redirect; }
+            set { _redirect = value; }
         }
 
         string _visitSessionCode;
@@ -235,7 +235,7 @@ namespace CSP.APIV2Modules
 
             Init();
 
-            R_runworkflow(AuthToken, studyProtocolName, DOM, genderCode, localityCode, postError, post, visitSessionCode, supervisorid, TestIdentifier, externalid, mode);
+            R_runworkflow(AuthToken, studyProtocolName, DOM, genderCode, localityCode, redirectError, redirect, visitSessionCode, supervisorid, TestIdentifier, externalid, mode);
             Delay.Milliseconds(0);
             
         }
