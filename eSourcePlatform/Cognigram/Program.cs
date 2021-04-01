b56fd7b66a02e68a1af6de1d9cf6efe2c676aa80
Frank Barilla <fbarilla@cogstate.com>
2021-04-01 13:59:32 +1100

    QC-162 - Created the Cognigram project and smart folders for Smoke and Regression Testing.

---
+++ eSourcePlatform/Cognigram/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by Ranorex
+ * User: fbarilla
+ * Date: 01-Apr-21
+ * Time: 1:53 PM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+
+using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+using Ranorex;
+
+namespace Cognigram
+{
+	class Program
+	{
+		[STAThread]
+		public static int Main(string[] args)
+		{	
+			// TODO: Add Code to call testing functions - for example:	
+			// Automation.Start()
+			
+            return 0;
+		}
+	}
+}
